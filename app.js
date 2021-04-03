79a366219d5a427c287cfd40bb1e5092827eaebc
Abir Taheer <abir@taheer.me>
2021-04-03 05:19:36 -0400

    complete

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const app = express();
-const logger = require('./middleware/logger');
-const session = require('./middleware/session');
-const parsers = require('./middleware/parsers');
-const errorHandler = require('./middleware/errorHandler');
-const apolloServer = require("./graphql");
+const basicAuth = require('express-basic-auth');
 
-app.use(logger);
+let status = true;
 
-app.use(session);
+app.get('/api/status', (req, res, next) => {
+	if (
+		req.query.password === process.env.PASSWORD &&
+		req.query.user === process.env.USER
+	) {
+		res.send(String(status));
+	} else {
+		next();
+	}
+});
 
-app.use(parsers);
+function unauthorizedResponse(req) {
+	return req.auth
+		? 'Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected'
+		: 'No credentials provided';
+}
 
-apolloServer.applyMiddleware({app, path: '/graphql', cors: false});
+app.use(
+	basicAuth({
+		users: { [process.env.USER]: process.env.PASSWORD },
+		challenge: true,
+		unauthorizedResponse
+	})
+);
 
-app.use(errorHandler);
+app.get('/api/status', (req, res) => res.send(String(status)));
+
+app.get('/api/disable', (req, res) => {
+	status = false;
+	res.send('successfully disabled');
+});
+
+app.get('/api/enable', (req, res) => {
+	status = true;
+	res.send('successfully enabled');
+});
+
+app.get('/', (req, res) => {
+	res.send(`
+		Current Status: ${String(status)}
+		<form action="/api/enable">
+			<button>Enable</button>
+		</form><br/><br/>
+		<form action='/api/disable'>
+			<button>Disable</button>
+		</form>
+	`);
+});
 
 module.exports = app;
